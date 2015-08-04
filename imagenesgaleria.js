58ecd9156b91a4209a6f3c0cef80a79ddb159a66
jeraldin27 <tatisjeral_1995@hotmail.com>
2015-08-03 20:35:10 -0500

    jeral

---
+++ imagenesgaleria.js
@@ -0,0 +1,23 @@
+function cambio0(){
+	document.getElementById('imgprincipal').src='./imagenes/images0.jpg';
+}
+
+function cambio1(){
+	document.getElementById('imgprincipal').src='./imagenes/images1.jpg';
+}
+
+function cambio2(){
+	document.getElementById('imgprincipal').src='./imagenes/images2.jpg';
+}
+
+function cambio3(){
+	document.getElementById('imgprincipal').src='./imagenes/images3.jpg';
+}
+
+function cambio4(){
+	document.getElementById('imgprincipal').src='./imagenes/images4.png';
+}
+
+function volver(){
+	document.getElementById('imgprincipal').src='./imagenes/images.jpg';
+}
